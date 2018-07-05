9ce8af9825049f3ff36b9ea5386c82b1e1e57dc0
András Veres-Szentkirályi <vsza@vsza.hu>
2018-07-05 17:31:45 +0200

    certcloner: added support for leaf cert cloning

--- certcloner.erl
+++ certcloner.erl
@@ -1,5 +1,5 @@
 -module(certcloner).
--export([clone/3]).
+-export([clone/3, clone/4]).
 
 -include_lib("public_key/include/public_key.hrl").
 
@@ -18,3 +18,22 @@ clone(CertPemFileName, KeyPemFileName, DerOutFileName) ->
 	NewLeafCert = Cert#'OTPCertificate'.tbsCertificate#'OTPTBSCertificate'{
 		subjectPublicKeyInfo = NewLeafCertSPKI},
 	file:write_file(DerOutFileName, public_key:pkix_sign(NewLeafCert, Key)).
+
+clone(CertPemFileName, IssuerKeyPemFileName, SubjectKeyPemFileName, DerOutFileName) ->
+	{ok, PemBin} = file:read_file(CertPemFileName),
+	[{'Certificate', DER, not_encrypted} | _] = public_key:pem_decode(PemBin),
+	{ok, IKPemBin} = file:read_file(IssuerKeyPemFileName),
+	[{'RSAPrivateKey' = IT, IRSA, not_encrypted} | _] = public_key:pem_decode(IKPemBin),
+	IssuerKey = public_key:der_decode(IT, IRSA),
+	{ok, SKPemBin} = file:read_file(SubjectKeyPemFileName),
+	[{'RSAPrivateKey' = ST, SRSA, not_encrypted} | _] = public_key:pem_decode(SKPemBin),
+	SubjectKey = public_key:der_decode(ST, SRSA),
+	Cert = public_key:pkix_decode_cert(DER, otp),
+	#'RSAPrivateKey'{modulus = Mod, publicExponent = Exp} = SubjectKey,
+	NewLeafCertSPKI = Cert#'OTPCertificate'.tbsCertificate
+		#'OTPTBSCertificate'.subjectPublicKeyInfo
+		#'OTPSubjectPublicKeyInfo'{
+		   subjectPublicKey = #'RSAPublicKey'{modulus = Mod, publicExponent = Exp}},
+	NewLeafCert = Cert#'OTPCertificate'.tbsCertificate#'OTPTBSCertificate'{
+		subjectPublicKeyInfo = NewLeafCertSPKI},
+	file:write_file(DerOutFileName, public_key:pkix_sign(NewLeafCert, IssuerKey)).
